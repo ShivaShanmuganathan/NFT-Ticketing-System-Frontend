3bed6c2ba843770c5a44cc44b60157deee75741e
Shiva <shanshiva@outlook.com>
2022-03-01 16:40:42 +0530

    Added Buy Function

--- src/App.js
+++ src/App.js
@@ -270,7 +270,9 @@ function App() {
           <Routes>
             <Route
               path="/"
-              element={<Buy />}
+              element={<Buy 
+                connectedContract={connectedContract}
+              />}
             />
 
             <Route
--- src/pages/Buy.js
+++ src/pages/Buy.js
@@ -4,9 +4,93 @@ import {
   Heading,
   Text,
   Flex,
+  useToast
 } from "@chakra-ui/react";
+import {useState, useEffect} from "react"
 
-function Buy() {
+function Buy({connectedContract}) {
+  const toast = useToast();
+  const [totalTicketCount, setTotalTicketCount] = useState(null);
+
+  const [availableTicketCount, setAvailableTicketCount] = useState(null);
+
+  const [buyTxnPending, setBuyTxnPending] = useState(false);
+
+  const getAvailableTicketCount = async() => {
+
+    try{
+      const count = await connectedContract.displayAvailableTickets();
+      setAvailableTicketCount(count.toNumber());
+    }
+
+    catch(error) {
+      console.log(error);
+    }
+    
+  }
+
+  const getTotalTicketCount = async() => {
+
+    try{
+      const count = await connectedContract.displayTotalTickets();
+      setTotalTicketCount(count.toNumber());
+    }
+
+    catch(error) {
+      console.log(error);
+    }
+    
+  }
+
+  const buyTicket = async () => {
+    try {
+      
+      if (!connectedContract) return;
+
+      setBuyTxnPending(true);
+      const buyTxn =
+        await connectedContract.mint({
+          value: `${0.1 * 10 ** 18}`,
+        });
+
+      await buyTxn.wait();
+      setBuyTxnPending(false);
+      toast({
+        title: "Success!",
+        description: (
+          <a
+            href={`https://rinkeby.etherscan.io/tx/${buyTxn.hash}`}
+            target="_blank"
+            rel="nofollow noreferrer"
+          >
+            Checkout the transaction on
+            Etherscan
+          </a>
+        ),
+        status: "success",
+        variant: "subtle",
+      });
+    } 
+    catch (err) {
+      console.log(err);
+      setBuyTxnPending(false);
+      toast({
+        title: "Failed.",
+        description: err,
+        status: "error",
+        variant: "subtle",
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (!connectedContract) return;
+
+    getAvailableTicketCount();
+    getTotalTicketCount();
+  });
+
+
   return (
     <>
       <Heading mb={4}>
@@ -26,6 +110,8 @@ function Buy() {
       >
         <ButtonGroup mb={4}>
           <Button
+            onClick={buyTicket}
+            isLoading={buyTxnPending}
             loadingText="Pending"
             size="lg"
             colorScheme="teal"
@@ -33,6 +119,15 @@ function Buy() {
             Buy Ticket
           </Button>
         </ButtonGroup>
+
+        {availableTicketCount &&
+          totalTicketCount && (
+            <Text>
+              {availableTicketCount} of{" "}
+              {totalTicketCount} minted!
+            </Text>
+          )}
+
       </Flex>
     </>
   );
